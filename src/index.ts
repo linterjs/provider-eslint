f5558393f221d80f2bc1126506d20056bbd173f3
Simon Fridlund <simon@fridlund.email>
2017-12-06 19:12:24 +0100

    Add an adapter that does nothing

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { registerLinter, LinterAdapter, LinterFactory, LintOutput } from "@linter/core";
+import { CLIEngine } from "eslint";
+
+const adapter: LinterAdapter = {
+  format({ filePath, text }) {
+    // TODO: Format all the things...
+    return {};
+  },
+  lint({ filePath, text }) {
+    // TOOD: Lint all the things...
+    return {};
+  },
+};
+
+const linterFactory: LinterFactory = () => adapter;
+
+registerLinter("eslint", linterFactory);
