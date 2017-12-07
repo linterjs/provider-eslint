f159ed046c23ca7937a84862d317258cca595822
Simon Fridlund <simon@fridlund.email>
2017-12-07 21:57:50 +0100

    Add first implementation of lint
    Simple solution that requires an eslint config file.

--- src/lint.ts
+++ src/lint.ts
@@ -1,9 +1,13 @@
 import { LinterAdapterLintInput, LinterAdapterLintOutput } from "@linter/core";
+import { CLIEngine } from "eslint";
 
 export function lint({
   filePath,
   text
 }: LinterAdapterLintInput): LinterAdapterLintOutput {
+  const cliEngine = new CLIEngine();
+  const result = cliEngine.executeOnText(text, filePath);
+  console.log(result);
   // TODO: Figure out result format
   return {};
 }
