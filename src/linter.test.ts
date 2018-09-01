5220dd30dacc36d6d6aaa9af7c4356b37d0f41ef
Simon Fridlund <simon@fridlund.email>
2018-09-02 00:08:35 +0200

    Update tests containing filePath to work in different environments
    Snapshots had filePath specific to environment where snapshot where generated the first time

--- src/linter.test.ts
+++ src/linter.test.ts
@@ -52,8 +52,14 @@ describe("Format", () => {
     const linter = await linterProvider.linterFactory();
 
     expect(
-      linter.format({ filePath: "foo.js", text: 'var bar = "baz"' }),
-    ).toMatchSnapshot();
+      linter.format({ filePath: "./foo.js", text: 'var bar = "baz"' }),
+    ).toEqual({
+      errorCount: 0,
+      filePath: `${process.cwd()}/foo.js`,
+      messages: [],
+      output: "var bar = 'baz';",
+      warningCount: 0,
+    });
   });
 
   test("with already formatted text", async () => {
@@ -99,12 +105,50 @@ describe("Lint", () => {
           semi: ["warn"],
         },
       }),
-      ".eslintignore": "foo.js",
     });
 
     const linter = await linterProvider.linterFactory();
 
     expect(linter.lint({ text: 'var foo = "bar"' })).toMatchSnapshot();
+  });
+
+  test("with text and filePath", async () => {
+    fs.mock({
+      ".eslintrc": JSON.stringify({
+        rules: {
+          quotes: ["error", "single"],
+          semi: ["warn"],
+        },
+      }),
+    });
+
+    const linter = await linterProvider.linterFactory();
+
+    expect(
+      linter.lint({ filePath: "foo.js", text: 'var bar = "baz"' }),
+    ).toEqual({
+      errorCount: 1,
+      filePath: `${process.cwd()}/foo.js`,
+      messages: [
+        {
+          column: 11,
+          endColumn: 16,
+          endLine: 1,
+          line: 1,
+          message: "Strings must use singlequote.",
+          ruleId: "quotes",
+          severity: 2,
+        },
+        {
+          column: 16,
+          line: 1,
+          message: "Missing semicolon.",
+          ruleId: "semi",
+          severity: 1,
+        },
+      ],
+      warningCount: 1,
+    });
   });
 
   test("with syntax error", async () => {
@@ -115,7 +159,6 @@ describe("Lint", () => {
           semi: ["warn"],
         },
       }),
-      ".eslintignore": "foo.js",
     });
 
     const linter = await linterProvider.linterFactory();
