ff11a0641ef8b200b84b40787fa9b75cb7088227
Simon Fridlund <simon@fridlund.email>
2018-09-01 20:59:26 +0200

    Update jest

--- jest.config.js
+++ jest.config.js
@@ -13,12 +13,10 @@ module.exports = {
       useBabelrc: true,
     },
   },
-  mapCoverage: true,
   moduleFileExtensions: ["js", "jsx", "json", "ts", "tsx"],
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.(j|t)s?(x)", "**/?(*.)(spec|test).(j|t)s?(x)"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  setupFiles: ["jest-plugin-fs/setup"],
 };
